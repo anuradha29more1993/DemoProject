dbdb8f3f134e519080a2c4c4b0c07af8e0879a4c
anuradha <anuradha29more1993@gmail.com>
2023-08-30 15:50:09 +0530

    Addition of 1 to 10 numbers

---
+++ Sample/src/com/velocity/Test.java
@@ -0,0 +1,13 @@
+package com.velocity;
+
+public class Test {
+	public static void main(String[] args) {
+		int add=0;
+		for(int i=1;i<=10;i++) {
+		  add=add+i;
+		  System.out.println("Addition is"+add);
+			
+		}
+	}
+
+}
